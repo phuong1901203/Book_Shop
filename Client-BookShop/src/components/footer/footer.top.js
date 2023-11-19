5939c651452138a64f0934ff3768cff12ceb7a97
phuong1901203 <vietphuong19012003@gmail.com>
2023-11-20 00:36:40 +0700

    commit

---
+++ Client-BookShop/src/components/footer/footer.top.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+const FooterTop = () => (
+    <div className="footer-top">
+        <div className="container">
+            <div className="row">
+                <div className="col-sm-3">
+                    <div className="companyinfo">
+                        <h2><span>BookShop</span></h2>
+                        <p>Bookshop là một website chuyên bán sách, bên chúng tôi có rất nhiều thể loại sách để phục vụ cho quý khách!
+                        </p>
+                    </div>
+                </div>
+                <div className="col-sm-6"></div>
+          
+                <div className="col-sm-3">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+)
+export default FooterTop
